739bc5dd0061a14fd95825a54b987489e6a2d45c
lenovo <ziruiqiao@cmail.carleton.ca>
2025-03-01 19:31:38 -0500

    update data_process_tools

--- app/src/server.py
+++ app/src/server.py
@@ -33,8 +33,13 @@ async def analyze(file: UploadFile = File(...)):
 
 
 @app.post("/get-suggest/")
-async def get_llm_suggest(data: dict):
+async def get_llm_suggest(request:Request):
     try:
-        
+        data = await request.json()
+        data.pop('timesES')
+        data.pop('nni_seq')
+        for k, v in data['hrv_results'].items():
+            if k.startswith(('SDNN', 'RMSSD', 'PNN50')):
+                data[k] = data['hrv_results'].pop(k)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Get LLM Suggestion failed: {str(e)}")
